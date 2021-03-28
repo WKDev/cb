7361e45b05b07d0f4cdfa4328ffec90cdb4b8de6
wkdev <chanhyukman@gmail.com>
2021-03-28 15:48:48 +0900

    second build

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.end("Welcome! This is Cranberry BackendServer_test");
+  //   res.end("Welcome! This is Cranberry BackendServer_test");
+  res.sendFile(path.join(__dirname + "../client/build/index.html"));
 });
 
 module.exports = router;
