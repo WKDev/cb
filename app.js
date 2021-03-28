47ba5c3ab29ace518e48a2f2bb1f54c085b026d6
wkdev <chanhyukman@gmail.com>
2021-03-28 15:34:58 +0900

    first build

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-var express = require("express");
-var app = express();
-var cors = require("cors");
-var session = require("express-session");
-
-var dotenv = require("dotenv");
+const express = require("express"); // Express 서버
+const cors = require("cors"); // 통신 간 보안 문제 해결을 위해 포함됨
+const session = require("express-session"); // 세션 처리
+const dotenv = require("dotenv"); // key 값 보관하고 관리하는 데 사용됨. 지금은 사용 안하는중
+const path = require("path"); // static한 파일(React Build 폴더)에 접근할 수 있도록 Route 설정하는데 필요
+const app = express();
 
 // React와의 통신을 위해 cors, express.json 사용
+
 app.use(cors());
 app.use(express.json());
 
@@ -28,7 +29,6 @@ app.use(indexRouter);
 app.use("/users", usersRouter);
 
 //인증 여부 확인
-//세션 발급해주기
 app.post("/ref_sess", (req, res) => {
   var sess = req.session;
   sess.save();
@@ -51,8 +51,16 @@ app.post("/ref_sess", (req, res) => {
   }
 });
 
-// portnumber를 3002로 지정
+// portnumber를 5000으로 지정
 const port = process.env.PORT || 5000;
 
 // 3002번 포트넘버를 가진 서버 생성
 app.listen(port, () => console.log(`listening on port ${port}!`));
+
+//React Static 파일 제공하기
+app.use(express.static(path.join(__dirname, "client/build")));
+
+// 라우트 설정
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.end("Welcome! This is Cranberry BackendServer");
+  res.end("Welcome! This is Cranberry BackendServer_test");
 });
 
 module.exports = router;
